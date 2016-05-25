2213432d1d2967cef433b401e81846f511553cff
ckcks12 <wyoducc@gmail.com>
2016-05-25 13:00:34 +0900

    sync server and retrieve the pages' body text

--- ChromeExtension/skimmer.js
+++ ChromeExtension/skimmer.js
@@ -1,11 +1,41 @@
-var r_arr = document.getElementsByClassName("r");
-if( r_arr.length > 0 )
+(function skimmer()
 {
-	for( var i=0; i<r_arr.length; i++ )
+	var r_arr = document.getElementsByClassName("r");
+	if( r_arr.length > 0 )
 	{
-		r_arr[i].parentNode.innerHTML = word("화면크기") + word("충동구매") + word("골드") + r_arr[i].parentNode.innerHTML;
+		for( var i=0; i<r_arr.length; i++ )
+		{
+			r_arr[i].parentNode.innerHTML = word("화면크기") + word("충동구매") + word("골드") + r_arr[i].parentNode.innerHTML;
+		}
 	}
-}
+});
+//document.querySelectorAll(".r")[0].getElementsByTagName("a")[0].href
+(function skimmer_test()
+{
+	var xhr = new XMLHttpRequest();
+	xhr.onreadystatechange = function()
+	{
+		if( xhr.readyState == 4 )
+		{
+			console.log( xhr.responseText );
+		}
+	};
+
+	var r_arr = document.getElementsByClassName("r");
+	/*
+	if( r_arr.length > 0 )
+	{
+		for( var i=0; i<r_arr.length; i++ )
+		{
+			r_arr[i].parentNode.innerHTML = word("화면크기") + word("충동구매") + word("골드") + r_arr[i].parentNode.innerHTML;
+		}
+	}
+	*/
+	var url = r_arr[0].getElementsByTagName("a")[0].href;
+
+	xhr.open("GET", "//skimmer.ckcks12.com/skim.php?url="+url);
+	xhr.send();
+})();
 
 function log(s)
 {
