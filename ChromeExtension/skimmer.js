4a48d340bb3b67fe0c3f259e8a62d134b92d98f7
ckcks12 <wyoducc@gmail.com>
2016-07-15 23:47:28 +0900

    skimm 2

--- ChromeExtension/skimmer.js
+++ ChromeExtension/skimmer.js
@@ -10,9 +10,13 @@ function skimmerDaemon()
 	wait();
 	if( document.querySelector("#skim") == null )
 	{
-		skimmer();
+		if( ! window.skimming )
+		{
+			skimmer();
+			window.skimming = true;
+		}
 	}
-	setTimeout(skimmerDaemon, 3000);
+	setTimeout(skimmerDaemon, 5000);
 }
 
 function skim(word)
@@ -31,31 +35,32 @@ function skim(word)
 /*
 function skimmer()
 {  
-	var r_arr = document.getElementsByClassName("r"); 
-	if( r_arr.length <= 0 )
+	var window.r_arr = document.getElementsByClassName("r"); 
+	if( window.r_arr.length <= 0 )
 	{	
 		return setTimeout(skimmer, 1000);
 	} 
 	else
 	{
-		for( var i=0; i<r_arr.length; i++ )
+		for( var i=0; i<window.r_arr.length; i++ )
 		{
-			r_arr[i].parentNode.innerHTML = skim(["화면", "골드", "어에에예예", "오잉~?"]) + r_arr[i].parentNode.innerHTML;
+			window.r_arr[i].parentNode.innerHTML = skim(["화면", "골드", "어에에예예", "오잉~?"]) + window.r_arr[i].parentNode.innerHTML;
 		}
 	}
 }
 */
 
+window.skimmed_data = [];
 function skimmer()
 {
-	var r_arr = document.getElementsByClassName("r"); 
-	if( r_arr.length <= 0 )
+	window.r_arr = document.getElementsByClassName("r"); 
+	if( window.r_arr.length <= 0 )
 	{	
 		return setTimeout(skimmer, 1000);
 	} 
 
 	var xhr = [];
-	for( var i=0; i<r_arr.length; i++ )
+	for( var i=0; i<window.r_arr.length; i++ )
 	{
 		xhr[i] = new XMLHttpRequest();
 		xhr[i].idx = i;
@@ -64,16 +69,93 @@ function skimmer()
 			return () =>
 			{
 				if( xhr[k].readyState == 4 )
-				{ 
-					console.log("xhr[" + k + "] response : " + xhr[k].responseText);
-					var data = JSON.parse(xhr[k].responseText);
-					r_arr[k].parentNode.innerHTML = skim(data) + r_arr[k].parentNode.innerHTML;
+				{
+					//console.log("xhr[" + k + "] response : " + xhr[k].responseText);
+					console.log("xhr[" + k + "] : " + xhr[k].responseText.length);
+					if( xhr[k].responseText.length <= 10 )
+					{
+						var data = JSON.parse('{"status": "-1"}');
+					}
+					else
+					{
+						var data = JSON.parse(xhr[k].responseText);
+					}
+
+					if( window.skimmed_data.length < window.r_arr.length )
+					{
+						window.skimmed_data[window.skimmed_data.length] = data;
+					}
+					if( window.skimmed_data.length == window.r_arr.length )
+					{
+						for( var j=0; j<window.skimmed_data.length; j++ )
+						{
+							if( window.skimmed_data[j].status != 1 )
+							{
+								window.skimmed_data.splice(j, 1);
+								j--;
+							}
+						}
+
+						window.noun_list = [];
+						window.noun_cnt = [];
+
+						for( var j=0; j<window.skimmed_data.length-1; j++ )
+						{
+							for( var l=j+1; l<window.skimmed_data.length; l++ )
+							{
+								for( var jj=0; jj<window.skimmed_data[j].data.length; jj++ )
+								{
+									for( var ll=0; ll<window.skimmed_data[l].data.length; ll++ )
+									{
+										if( window.skimmed_data[j].data[jj].noun == window.skimmed_data[l].data[ll].noun )
+										{
+											window.noun_exist = false;
+											for( window.noun_idx=0; window.noun_idx<window.noun_list.length; window.noun_idx++ )
+											{
+												if( window.noun_list[window.noun_idx] == window.skimmed_data[j].data[jj].noun )
+												{
+													window.noun_cnt[window.noun_idx]++;
+													window.noun_exist = true;
+													break;
+												}
+											}
+
+											if( ! window.noun_exist )
+											{
+												window.noun_list[window.noun_list.length] = window.skimmed_data[j].data[jj].noun;
+												window.noun_cnt[window.noun_list.length] = 1;
+											}
+										}
+									}
+								}
+							}
+						}
+
+						console.log(window.noun_list);
+						console.log(window.noun_cnt);
+						for( var j=0; j<window.skimmed_data.length; j++ )
+						{
+							for( var l=0; l<window.skimmed_data[j].data.length; l++ )
+							{
+								var data = window.skimmed_data[j].data[l];
+								for( var m=0; m<window.noun_list.length; m++ )
+								{
+									if( data.noun == window.noun_list[m] )
+									{
+										console.log((j+1) + "번째 글의 [" + data.noun + "] 요약 : " + data.modifiers);
+									}
+								}
+							}
+						}
+						window.r_arr[k].parentNode.innerHTML = skim([" "]) + window.r_arr[k].parentNode.innerHTML;
+						window.skimming = false;
+					}
 				}
 			}; 
 		})(i);
-		var url = r_arr[i].getElementsByTagName("a")[0].href;
+		var url = window.r_arr[i].getElementsByTagName("a")[0].href;
 
-		xhr[i].open("GET", "//skimmer.ckcks12.com/skim.php?url="+url);
+		xhr[i].open("GET", "//skimmer.ckcks12.com/skim2.php?url="+url);
 		xhr[i].send();
 		console.log("xhr[" + i + "] : " + url); 
 	}
